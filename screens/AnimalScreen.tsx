4b9671b8fa103c45964dafd8a380cf7ffba9ffda
Guilherme Passos <guilhermep@esparta.io>
2021-10-20 20:51:48 -0300

    using PageContainer component

--- screens/AnimalScreen.tsx
+++ screens/AnimalScreen.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { StyleSheet } from "react-native";
+import { PageContainer } from '../components/PageContainer';
 
 import { Text, View } from "../components/Themed";
 import { RootStackScreenProps } from "../types";
 
-const teste = [0, 1, 2]
-
 export function AnimalScreen({ navigation, route }: RootStackScreenProps<'Animal'>) {
     return (
-        <Text>Animal Screen</Text>
+        <PageContainer 
+            onBack={() => navigation.goBack()}
+
+        >
+            <Text>Animal Screen</Text>
+        </PageContainer>
     );
 }
